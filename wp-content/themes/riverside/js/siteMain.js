2b1037e2dde06a2358af5de85da430925974f3b2
lthanhbinh <lthanhbinh@tma.com.vn>
2017-11-29 13:27:50 +0700

    a

--- wp-content/themes/riverside/js/siteMain.js
+++ wp-content/themes/riverside/js/siteMain.js
@@ -9,11 +9,9 @@ var siteMain = (function() {
 	}
 
     function checkScrolledHeader(){
-        if(jQuery('header.page_header').hasClass('scrolled')){
-            console.log('has scrolled')
+        if(jQuery('header.page_header').hasClass('scrolled') || jQuery(window).width() <= 992){
             jQuery('header.page_header .header_top_bottom_holder .header_bottom').css('background-color',jQuery('header.page_header').attr('data-background'));
         }else {
-            console.log('no scrolled')
             jQuery('header.page_header .header_top_bottom_holder .header_bottom').css('background-color','rgba(0, 0, 0, 0.00)');
         }
     }
