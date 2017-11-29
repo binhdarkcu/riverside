26c22112721f73b27647238b5e9213a98d362b66
lthanhbinh <lthanhbinh@tma.com.vn>
2017-11-29 18:01:40 +0700

    plugin

--- wp-content/themes/riverside/tpl-info-project.php
+++ wp-content/themes/riverside/tpl-info-project.php
@@ -14,6 +14,10 @@
             </div>
         </div>
     </div>
+    <?php
+        $pageId = get_id_by_slug('gioi-thieu', 'page');
+        $imageProject = get_field('intro_image_project', $pageId);
+    ?>
     <div class=" section_inner clearfix">
         <div class='section_inner_margin clearfix'>
             <div class="wpb_column vc_column_container vc_col-sm-6">
@@ -23,7 +27,7 @@
                             <div class="wpb_wrapper">
 
                                 <a href="images/bg-home-about.jpg" target="_self">
-                                    <div class="vc_single_image-wrapper   vc_box_border_grey"><img width="555" height="403" src="images/bg-home-about.jpg" class="vc_single_image-img attachment-full" alt="" /></div>
+                                    <div class="vc_single_image-wrapper vc_box_border_grey"><img width="555" height="403" src="<?php echo $imageProject;?>" class="vc_single_image-img attachment-full" alt="" /></div>
                                 </a>
                             </div>
                         </div>
@@ -36,30 +40,27 @@
                         <div class="wpb_raw_code wpb_content_element wpb_raw_html vc_custom_1493349248837 ls-home-about">
                             <div class="wpb_wrapper">
                                 <div class="row">
-                                    <div class="col-xs-6 col-sm-6 item item1">
-                                        <div class="title2">Tổng diện tích dự án</div>
-                                        <div class="desc">16.800 m²</div>
-                                    </div>
-                                    <div class="col-xs-6 col-sm-6 item item2">
-                                        <div class="title2">Diện tích xây dựng</div>
-                                        <div class="desc">3.604 m²</div>
-                                    </div>
-                                    <div class="col-xs-6 col-sm-6 item item3">
-                                        <div class="title2">Mật độ xây dựng</div>
-                                        <div class="desc">19%</div>
-                                    </div>
-                                    <div class="col-xs-6 col-sm-6 item item4">
-                                        <div class="title2">Tháp &amp; Tầng cao</div>
-                                        <div class="desc">3<span> Tháp &amp; </span>22<span> Tầng</span></div>
-                                    </div>
-                                    <div class="col-xs-6 col-sm-6 item item5">
-                                        <div class="title2">Công viên cây xanh</div>
-                                        <div class="desc">8.000 m²</div>
-                                    </div>
-                                    <div class="col-xs-6 col-sm-6 item item6">
-                                        <div class="title2">Tổng vốn đầu tư</div>
-                                        <div class="desc">1.286 tỉ</div>
-                                    </div>
+                                    <?php
+                                    $i=0;
+                                    if( have_rows('intro_info_project', $pageId) ): ?>
+
+                                    	<?php while( have_rows('intro_info_project', $pageId) ): the_row();
+                                            $i++;
+                                    		// vars
+                                    		$intro_project_title = get_sub_field('intro_project_title', $pageId);
+                                    		$intro_project_number = get_sub_field('intro_number_project', $pageId);
+                                    		$intro_icon = get_sub_field('intro_icon_project', $pageId);
+                                    		?>
+
+                                            <div class="col-xs-6 col-sm-6 item item<?php echo $i;?>">
+                                                <div class="title2"><?php echo $intro_project_title;?></div>
+                                                <div class="desc" style="background: url(<?php echo $intro_icon?>) no-repeat bottom 17px right"><?php echo $intro_project_number;?></div>
+                                            </div>
+
+                                    	<?php endwhile; ?>
+
+                                    <?php endif; ?>
+
                                 </div>
                             </div>
                         </div>
--- wp-content/themes/riverside/tpl-intro.php
+++ wp-content/themes/riverside/tpl-intro.php
@@ -8,17 +8,17 @@
             <div class="wpb_column vc_column_container vc_col-sm-12">
                 <div class="vc_column-inner ">
                     <div class="wpb_wrapper">
-                        <div class="wpb_text_column wpb_content_element  hide">
-                            <div class="wpb_wrapper">
-                                <p><span style="color: #002f1e; position: absolute;">Dự án căn hộ cao cấp quận 8 Diamond Lotus Riverside đã bàn giao nhà Block A và B. Nhận ngay báo giá mới nhất Block C. Đừng bỏ qua cơ hội tốt này. can ho diamond lotus reversdie, can ho quan 8, ban can ho diamond lotus, du an diamond lotus q8, căn hộ quận 8, can mua nha quan 8, ban nha tra gop quan 8, mua nha tra gop quan 8, mua nhà ở quận 8, chung cư trả góp quận 8, ban can ho quan 8, nha ban o quan 8, can ho q8, bat dong san quan 8, du an can ho quan 8, can ho tra gop quan 8, căn hộ cao cấp quận 8, mua nha q8. </span></p>
-
-                            </div>
-                        </div>
-
+                        <?php
+                            $pageId = get_id_by_slug('gioi-thieu', 'page');
+                            $pageTitle = get_field('intro_subtitle', $pageId);
+                            $videoUrl = get_field('intro_video', $pageId);
+                            $getPost = get_post($pageId);
+                            $introContent = $getPost->post_content;
+                        ?>
                         <div class="wpb_raw_code wpb_content_element wpb_raw_html vc_custom_1510560840240">
                             <div class="wpb_wrapper">
                                 <div class="page-branch-title text-center">
-                                    <h2 class="title1 ">CĂN HỘ XANH ĐẠT TIÊU CHUẨN LEED</h2></div>
+                                    <h2 class="title1 "><?php echo $pageTitle;?></h2></div>
                             </div>
                         </div>
 
@@ -26,69 +26,12 @@
                             <div class="wpb_wrapper">
 
                                 <div class="wpb_video_wrapper">
-                                    <iframe width="1060" height="596" src="https://www.youtube.com/embed/6Kavt1QGR1U?feature=oembed&wmode=opaque" frameborder="0" gesture="media" allowfullscreen></iframe>
+                                    <iframe width="1060" height="596" src="https://www.youtube.com/embed/<?php echo $videoUrl['vid']?>?feature=oembed&wmode=opaque" frameborder="0" gesture="media" allowfullscreen></iframe>
                                 </div>
                             </div>
                         </div>
-
-                        <div class="wpb_raw_code wpb_content_element wpb_raw_html">
-                            <div class="wpb_wrapper">
-                                <style>
-                                    body {
-                                        color: #fff!important;
-                                    }
-
-                                    p {
-                                        color: #fff!important;
-                                    }
-
-                                    p.wp-caption-text {
-                                        text-align: center;
-                                    }
-
-                                    .repoo div {
-                                        max-width: 100%!important;
-                                    }
-
-                                    .conkhi .modal-content {
-                                        top: 0
-                                    }
-
-                                    .modal-backdrop {
-                                        display: none;
-                                    }
-
-                                    .bor-hove:hover {
-                                        border-left-color: #d8c664!important;
-                                        border-right-color: #d8c664!important;
-                                        border-top-color: #d8c664!important;
-                                        border-bottom-color: #d8c664!important;
-                                    }
-
-                                    .wpcf7-validation-errors {
-                                        display: none!important;
-                                    }
-                                </style>
-                                <div class="page-branch-title text-center">
-                                    <h1 class="title1 ">DIAMOND LOTUS RIVERSIDE</h1></div>
-                            </div>
-                        </div>
-
-                        <div class="wpb_text_column wpb_content_element ">
-                            <div class="wpb_wrapper">
-                                <blockquote>
-                                    <h2 style="text-align: center;"><span style="color: #ffffff;">CĂN HỘ <strong>SINH THÁI</strong> VEN SÔNG ”</span><br />
-                                        <span style="color: #ffffff;">CÁCH CHỢ BẾN THÀNH <strong>5 PHÚT</strong></span></h2>
-                                </blockquote>
-
-                            </div>
-                        </div>
-                        <div class="wpb_text_column wpb_content_element  vc_custom_1493346096943">
-                            <div class="wpb_wrapper">
-                                <p style="text-align: center;"><img class="alignnone size-full wp-image-528" src="http://diamondlotusphuckhang.com.vn/wp-content/uploads/2017/04/LEED-Gold.png" alt="" width="170" height="170" /></p>
-
-                            </div>
-                        </div>
+                        <?php echo $introContent;?>
+                        <br/><br/>
                     </div>
                 </div>
             </div>
